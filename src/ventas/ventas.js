a85af4ad4dd1c60d3dff800f70f4b25e84fd7345
omkrfig <omkrfig@gmail.com>
2026-03-05 10:05:27 -0400

    feat: Calc total price with taxe

--- src/ventas/ventas.js
+++ src/ventas/ventas.js
@@ -3,4 +3,16 @@ function calcularTotal(cantidad, precio)
     return cantidad * precio;
 }
 
-export {calcularTotal};
+function calcularTotalImpuesto(cantidad, precio, estado)
+{
+    const estados = {'UT': 6.65/100}
+    let porcentajeimpuesto = 0;
+    let calc = 0;
+    if(estado === 'UT')
+    {
+        porcentajeimpuesto = estados['UT']
+    }
+    calc = cantidad * precio * porcentajeimpuesto
+    return (cantidad * precio) + calc ;
+}
+export {calcularTotal, calcularTotalImpuesto};
--- src/ventas/ventas.test.js
+++ src/ventas/ventas.test.js
@@ -1,7 +1,11 @@
-import {calcularTotal} from './ventas.js'
+import {calcularTotal, calcularTotalImpuesto} from './ventas.js'
 
 describe('Sistema de ventas', () => {
   it('Deberia mostrar 300 al ingresar cantidad = 100 y precio = 3', () => {
     expect(calcularTotal(100,3)).toEqual(300);
   });
+  it('Deberia mostrar al ingresar cantidad = 400, precio = 100 y estado = UT', () => {
+    expect(calcularTotalImpuesto(400,100,'UT')).toEqual(42660);
+  });
+  
 });
